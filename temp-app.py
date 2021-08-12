3a4d44f45ba9a4594248a68c285d8cf421e49827
Matt Matheson <matt.matheson@outlook.com>
2021-08-12 15:32:58 +0000

    revert fixes

--- temp-app.py
+++ temp-app.py
@@ -1,33 +1,25 @@
-import argparse
-import json
 import time
-
+import json
 import requests
+import argparse
 
-TEMP_SENSOR = "/sys/bus/w1/devices/28-000008e233e8/w1_slave"
-HONEYCOMB_URL = "https://api.honeycomb.io/1/events/p70"
-
+TEMP_SENSOR="/sys/bus/w1/devices/28-000008e233e8/w1_slave"
+HONEYCOMB_URL="https://api.honeycomb.io/1/events/p70"
 
 def read_sensor():
-    with open(TEMP_SENSOR, "r") as sensor:
+    with open(TEMP_SENSOR, 'r') as sensor:
         return sensor.readlines()
-
-
+    
 def parse_sensor_output(sensor_output):
-    temperature = sensor_output[1].split("t=")[1]
-
+    temperature = sensor_output[1].split('t=')[1]
+    
     # Check that the temperature is not invalid
     if temperature != -1:
         temperature_celsius = round(float(temperature) / 1000.0, 1)
         temperature_fahrenheit = round((temperature_celsius * 1.8) + 32.0, 1)
-        return {
-            "celsius": temperature_celsius,
-            "fahrenheit": temperature_fahrenheit,
-            "ts": time.time(),
-        }
+        return {'celsius': temperature_celsius, 'fahrenheit': temperature_fahrenheit, 'ts':time.time()}
 
     return "Failed to parse"
-
 
 def get_temp():
     sensor_data = read_sensor()
@@ -37,14 +29,12 @@ def get_temp():
 
 def collect_temps(location):
     while True:
-        temp = get_temp()
+        temp = get_temp() 
         temp["location"] = location
         print(temp)
         hdrs = {"X-Honeycomb-Team": "beb254cd3be6f81f4b38cdea524bdeaf"}
         try:
-            requests.post(
-                HONEYCOMB_URL, data=json.dumps(temp), headers=hdrs, timeout=100
-            )
+            requests.post(HONEYCOMB_URL, data=json.dumps(temp), headers=hdrs, timeout=100)
         except Exception as E:
             print("Failed to post request: {} (time: {})".format(E, time.time()))
 
@@ -52,10 +42,9 @@ def collect_temps(location):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Set a location for these samples")
-    parser.add_argument(
-        "--location", dest="loc", help="set the location for the samples"
-    )
+    parser = argparse.ArgumentParser(description='Set a location for these samples')
+    parser.add_argument('--location', dest='loc', help='set the location for the samples')
     args = parser.parse_args()
     loc = args.loc
     collect_temps(loc)
+
